5b5246aa5249e2e6722fb679b9f205647a33d937
kojcinovica <kojcinovica@mts.rs>
2026-04-25 21:08:38 +0200

    Add ThemeManager
    - User chooses one of three themes for XO Buttons
    - XO Sprites add
    - ThemePopup updated with new buttons
    - XO Board updated with images, no more text elements

---
+++ Assets/Scripts/ThemeManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class ThemeManager : MonoBehaviour
+{
+    public static ThemeManager Instance;
+
+    public XOTheme[] themes;
+
+    public int selectedThemeIndex = 0;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+
+            DontDestroyOnLoad(gameObject);
+
+            LoadTheme();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public XOTheme GetCurrentTheme()
+    {
+        return themes[selectedThemeIndex];
+    }
+
+    public void SetTheme(int index)
+    {
+        selectedThemeIndex = index;
+
+        PlayerPrefs.SetInt(
+            "ThemeIndex",
+            index
+        );
+    }
+
+    void LoadTheme()
+    {
+        selectedThemeIndex =
+            PlayerPrefs.GetInt(
+                "ThemeIndex",
+                0
+            );
+    }
+}
---
+++ Assets/Scripts/XOTheme.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[CreateAssetMenu(
+    fileName = "XOTheme",
+    menuName = "XO Game/Theme"
+)]
+public class XOTheme : ScriptableObject
+{
+    public Sprite xSprite;
+    public Sprite oSprite;
+}
